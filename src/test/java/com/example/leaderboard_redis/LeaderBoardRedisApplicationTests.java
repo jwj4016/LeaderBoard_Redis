780c57a9b39e9d4bcd0ba89e8c0c30b1b4d853a5
J <jwj4016@gmail.com>
2023-04-06 13:42:54 +0900

    Initial commit

---
+++ src/test/java/com/example/leaderboard_redis/LeaderBoardRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaderboard_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderBoardRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
